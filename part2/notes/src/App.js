f1cd022e45025bec8aaa80ba15a199043fea5b75
Antonia Ilski <antonia.ilski@sevenval.com>
2019-09-26 12:43:26 +0200

    part 2b handling filtering in notes app

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Note from './components/Note';
 
-const App = ({notes}) => {
+const App = (props) => {
+    const [notes, setNotes] = useState(props.notes);
+    const [newNote, setNewNote] = useState(
+        'add new note …'
+    )
+    const [showAll, setShowAll] = useState(true);
+    const notesToShow = showAll ? notes : notes.filter(note => note.important === true)
     const rows = () =>
-        notes.map((note, index) => <Note key={index} note={note} />)
+        notesToShow.map((note, index) => <Note key={index} note={note} />);
+
+    const addNote = (event) => {
+        event.preventDefault();
+        const noteObject = {
+            content: newNote,
+            date: new Date().toISOString(),
+            important: Math.random() > 0.5,
+            id: notes.length + 1
+        }
+
+        setNotes(notes.concat(noteObject));
+        setNewNote('')
+    }
+
+    const handleNoteChange = (event) => {
+        setNewNote(event.target.value)
+    }
     return (
         <div>
-        <h1>Notes</h1>
-        <ul>
-            {rows()}
-        </ul>
+            <h1>Notes</h1>
+            <button onClick={() => setShowAll(!showAll)}>
+                show {showAll ? 'important': 'all'}
+            </button>
+            <ul>
+                {rows()}
+            </ul>
+            <form onSubmit={addNote}>
+                <input 
+                    value={newNote}
+                    onChange={handleNoteChange} />
+                <button type="submit">save</button>
+            </form>
         </div>
     )
 }
